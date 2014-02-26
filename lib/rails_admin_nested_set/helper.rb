93b55bd139d65936eea0ad19757edbb8f8b82bfe
glebtv <glebtv@gmail.com>
2014-02-26 15:34:55 +0400

    add image and enabled

--- lib/rails_admin_nested_set/helper.rb
+++ lib/rails_admin_nested_set/helper.rb
@@ -9,6 +9,14 @@ module RailsAdminNestedSet
       content + content_tag(:script, js.html_safe, type: 'text/javascript')
     end
 
+    def g_link(node, fv, on, badge)
+      link_to(
+        fv.html_safe,
+        toggle_path(model_name: @abstract_model, id: node.id, method: 'enabled', on: on.to_s),
+        method: :post,
+        class: 'badge ' + badge,
+      )
+    end
 
     def rails_admin_nested_set_builder(nodes, tree)
       nodes.map do |node|
@@ -17,8 +25,29 @@ module RailsAdminNestedSet
         content_tag :li, class: li_classes, :'data-id' => node.id do
           output = content_tag :div, 'drag', class: 'dd-handle dd3-handle'
           output+= content_tag :div, class: 'dd3-content' do
-            content = link_to @model_config.with(object: node).object_label, edit_path(@abstract_model, node.id)
-            content + content_tag(:div, action_links(node), class: 'pull-right links')
+            content = ''.html_safe
+
+            if node.respond_to?(:enabled) && respond_to?(:toggle_path)
+              content += case node.enabled
+                when nil
+                  g_link(node, '&#x2718;', 0, 'badge-important') + g_link(node, '&#x2713;', 1, 'badge-success')
+                when false
+                  g_link(node, '&#x2718;', 1, 'badge-important')
+                when true
+                  g_link(node, '&#x2713', 0, 'badge-success')
+                else
+                  %{<span class="badge">-</span>}
+              end.html_safe
+            end
+
+            content += link_to @model_config.with(object: node).object_label, edit_path(@abstract_model, node.id)
+            [:cover, :image].each do |mth|
+              if node.respond_to?(mth)
+                content += image_tag(node.send(mth), style: "max-height: 90px")
+              end
+            end
+            content += content_tag(:div, action_links(node), class: 'pull-right links')
+            content
           end
 
           children = tree.select{|elem| elem.parent_id == node.id}
